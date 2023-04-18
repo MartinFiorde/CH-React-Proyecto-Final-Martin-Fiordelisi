58c791acbd57b14386bf49878d18ae53fba995b4
MartinFiorde <fiorde_90@hotmail.com>
2023-04-17 21:22:27 -0300

    cambio para hosting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : '/CH-React-Proyecto-Final-Martin-Fiordelisi/',
   server: {
     port: 5174,
   },
